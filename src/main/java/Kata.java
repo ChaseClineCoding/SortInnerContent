73f6c40dafb42506cb17a249778fb16c75e29372
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-06-19 21:33:45 -0600

    finished solution

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,4 +1,23 @@
+import java.util.Arrays;
+
 public class Kata {
-    public static String sortTheInnerContent(String s) {
+
+    public static String sortTheInnerContent(String words) {
+        String[] splitWords = words.split(" ");
+        for (int i = 0; i < splitWords.length; i++) {
+            if (splitWords[i].length() > 2) {
+                splitWords[i] = splitWords[i].substring(0,1) + reverseSort(splitWords[i].substring(1,splitWords[i].length() - 1).toCharArray()) + splitWords[i].substring(splitWords[i].length()-1);
+            }
+        }
+        return String.join(" ", splitWords);
+    }
+
+    private static String reverseSort(char[] arr) {
+        Arrays.sort(arr);
+        StringBuilder result = new StringBuilder();
+        for (int i = arr.length-1; i >= 0; i--) {
+            result.append(arr[i]);
+        }
+        return result.toString();
     }
 }
